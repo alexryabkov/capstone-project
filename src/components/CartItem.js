620a2ed7c3c25e75de46cd9f0705f7c314d0dc64
Alexander Ryabkov <alexryabkov@gmail.com>
2020-06-17 16:39:57 +0300

    Fix compile warnings

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react"
+import React, { useContext } from "react"
 import PropTypes from "prop-types"
 import { Context } from "../Context"
 import useHover from "../hooks/useHover"
